41c31812b8d8675362f6b2825742445aaf29be91
“senpen1” <senpen65@gmail.com>
2024-03-03 15:05:40 +0900

    newjava1

---
+++ javastudy/Supervilan.java
@@ -0,0 +1,11 @@
+public class Supervilan extends Hero{
+    String name = "Dr.Evil";//悪役の名前を設定
+    boolean evilPlan;//悪役のフィールドを設定
+
+    public void evilPlan(){
+        this.evilPlan = true;
+        System.out.println(this.name + "は邪悪な計画を実行した!");
+    }
+
+
+}
